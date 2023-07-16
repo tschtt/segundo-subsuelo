06ef89788fb7e66162c5f18ef6afea358dcc4e92
tsch-throwaway <tsch.throwaway@gmail.com>
2023-07-15 22:58:04 -0300

    new landing page

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
  * @type {HTMLCanvasElement}
  */
 const canvas = document.querySelector('.canvas');
-
 /**
  * @type {CanvasRenderingContext2D}
  */
 const ctx = canvas.getContext('2d')
+/**
+ * @type {HTMLImageElement}
+ */
+const logo = document.querySelector('.logo')
+
 
 const state = {
   squares: [],
@@ -14,49 +18,62 @@ const state = {
     pos_x: -100,
     pos_y: -100,
     pressed: false,
+    click: false,
   }
 }
 
 function on_init () {
-  canvas.width = canvas.offsetWidth;
-  canvas.height = canvas.offsetHeight;
+  canvas.width = canvas.offsetWidth
+  canvas.height = canvas.offsetHeight
+
   on_draw()
 }
 
 function on_draw () {
-  if(canvas.width !== canvas.offsetWidth) {
-    canvas.width = canvas.offsetWidth;
-  }
+  // resize canvas
+  if(canvas.width !== canvas.offsetWidth) canvas.width = canvas.offsetWidth;
+  if(canvas.height !== canvas.offsetHeight) canvas.height = canvas.offsetHeight;
 
-  if(canvas.height !== canvas.offsetHeight) {
-    canvas.height = canvas.offsetHeight;
-  }
-
-  ctx.strokeStyle = 'hsla(120, 73%, 47%, .5)'
+  const expand_rate = 1;
   
-  ctx.fillStyle = 'hsla(0, 0%, 0%, .0018)'
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
-
-  ctx.filter = "blur(1px) hue-rotate(1deg)";
-
+  // render circle
+  ctx.beginPath();
+  ctx.lineWidth = 3
+  ctx.strokeStyle = 'hsla(120, 73%, 47%, .5)'
+  ctx.fillStyle = 'hsla(120, 73%, 47%, .5)'
+  ctx.roundRect(
+    (canvas.width / 2) - (logo.width / 2), 
+    (canvas.height / 2) - (logo.height / 2),
+    logo.width, 
+    logo.height, 
+    1000
+  )
+  ctx.stroke()
+  
+  // expand canvas
+  
+  // expand outwards
   ctx.drawImage(
     canvas, 
-    -1, 
-    -1, 
-    canvas.width + 2,
-    canvas.height + 2,
+    (expand_rate / 2) * -1,
+    (expand_rate / 2) * -1,
+    canvas.width + expand_rate,
+    canvas.height + expand_rate,
   )
 
-  // ctx.filter = "";
+  // expand inwards
+  // ctx.drawImage(
+  //   canvas, 
+  //   expand_rate / 2,
+  //   expand_rate / 2,
+  //   canvas.width - expand_rate,
+  //   canvas.height - expand_rate,
+  // )
+
   
-  ctx.beginPath();
-  
-  ctx.fillStyle = 'hsla(120, 73%, 47%, .5)'
-  ctx.roundRect(state.mouse.pos_x - 20, state.mouse.pos_y - 20, 40, 40, 100)
-  
-  // ctx.fillRect(0, 0, canvas.width, canvas.height)
-    
-  ctx.stroke()
+  ctx.fillStyle = 'hsla(0, 0%, 0%, .005)'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  ctx.filter = "blur(0.5px) hue-rotate(.5deg)";
 
   setTimeout(() => {
     requestAnimationFrame(on_draw)
@@ -70,10 +87,7 @@ function on_mouse_move (event) {
 
 function on_mouse_down (event) {
   state.mouse.pressed = true
-  // state.squares.push({
-  //   pos_x: event.pageX - canvas.offsetLeft,
-  //   pos_y: event.pageY - canvas.offsetTop,
-  // })
+  state.mouse.click = true
 }
 
 function on_mouse_up (event) {
@@ -83,5 +97,4 @@ function on_mouse_up (event) {
 window.addEventListener('mousemove', on_mouse_move)
 window.addEventListener('mousedown', on_mouse_down)
 window.addEventListener('mouseup', on_mouse_up)
-
-on_init()
+window.addEventListener('load', on_init)
