2145dd61b7551e438981a3925e55727e1b5c33ae
tsch-throwaway <tsch.throwaway@gmail.com>
2023-07-16 14:00:11 -0300

    rebuild main page

--- index.js
+++ index.js
@@ -2,15 +2,18 @@
  * @type {HTMLCanvasElement}
  */
 const canvas = document.querySelector('.canvas');
-/**
- * @type {CanvasRenderingContext2D}
- */
 const ctx = canvas.getContext('2d')
-/**
- * @type {HTMLImageElement}
- */
+
+const header = document.querySelector('.header')
 const logo = document.querySelector('.logo')
 
+// Helpers
+
+function map(n, start1, stop1, start2, stop2) {
+  return (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+};
+
+// Code
 
 const state = {
   squares: [],
@@ -23,9 +26,6 @@ const state = {
 }
 
 function on_init () {
-  canvas.width = canvas.offsetWidth
-  canvas.height = canvas.offsetHeight
-
   on_draw()
 }
 
@@ -35,23 +35,22 @@ function on_draw () {
   if(canvas.height !== canvas.offsetHeight) canvas.height = canvas.offsetHeight;
 
   const expand_rate = 1;
-  
-  // render circle
+  const logo_width = logo.offsetWidth
+  const header_height = header.offsetHeight
+
   ctx.beginPath();
-  ctx.lineWidth = 3
+  ctx.lineWidth = 10
   ctx.strokeStyle = 'hsla(120, 73%, 47%, .5)'
   ctx.fillStyle = 'hsla(120, 73%, 47%, .5)'
   ctx.roundRect(
-    (canvas.width / 2) - (logo.width / 2), 
-    (canvas.height / 2) - (logo.height / 2),
-    logo.width, 
-    logo.height, 
+    (canvas.width / 2) - (logo_width / 2), 
+    (header_height / 2) - (logo_width / 2),
+    logo_width,
+    logo_width,
     1000
   )
   ctx.stroke()
-  
-  // expand canvas
-  
+    
   // expand outwards
   ctx.drawImage(
     canvas, 
@@ -60,41 +59,14 @@ function on_draw () {
     canvas.width + expand_rate,
     canvas.height + expand_rate,
   )
-
-  // expand inwards
-  // ctx.drawImage(
-  //   canvas, 
-  //   expand_rate / 2,
-  //   expand_rate / 2,
-  //   canvas.width - expand_rate,
-  //   canvas.height - expand_rate,
-  // )
-
   
-  ctx.fillStyle = 'hsla(0, 0%, 0%, .005)'
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
-  ctx.filter = "blur(0.5px) hue-rotate(.5deg)";
+  // blur & hue rotation
+  ctx.filter = "blur(1px) hue-rotate(.5deg)";
 
+  // limit framerate to ~30fps
   setTimeout(() => {
     requestAnimationFrame(on_draw)
   }, 33);
 }
 
-function on_mouse_move (event) {
-  state.mouse.pos_x = event.pageX - canvas.offsetLeft
-  state.mouse.pos_y = event.pageY - canvas.offsetTop
-}
-
-function on_mouse_down (event) {
-  state.mouse.pressed = true
-  state.mouse.click = true
-}
-
-function on_mouse_up (event) {
-  state.mouse.pressed = false
-}
-
-window.addEventListener('mousemove', on_mouse_move)
-window.addEventListener('mousedown', on_mouse_down)
-window.addEventListener('mouseup', on_mouse_up)
 window.addEventListener('load', on_init)
